30ce87a3857295486fc33f2c517ff8aecf640ddf
jhaom2006 <jhaom2006@gmail.com>
2026-09-23 21:15:57 +0530

    Time: 1 ms (99.82%) | Memory: 46.8 MB (65.82%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        String[] answer = new String[n]; 
+        for(int i=0; i<n ; i++){
+            if((i+1)%3==0 && (i+1)%5==0){
+                answer[i] = "FizzBuzz";
+            }
+            else if ((i+1)%3 == 0){
+                answer[i] = "Fizz";
+            }
+             else if ((i+1)%5 == 0){
+                answer[i] = "Buzz";
+            }
+            else{
+                answer[i] =String.valueOf(i+1);
+            }
+        }
+    return Arrays.asList(answer);
+   }
+}
